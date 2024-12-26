1cf05015e007d6e0470f8651cbe0b086ce30083c
Ivan Perdomo <iperdomo2016@gmail.com>
2024-12-26 00:38:00 -0400

    fix min height on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import { CssBaseline, Box, ThemeProvider } from "@mui/material";
 import { darkTheme } from "./theme";
 import { Player } from "./Player";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const setVh = () => {
+      document.documentElement.style.setProperty(
+        "--vh",
+        `${window.innerHeight * 0.01}px`
+      );
+    };
+    setVh();
+    window.addEventListener("resize", setVh);
+    return () => window.removeEventListener("resize", setVh);
+  }, []);
+
   return (
     <ThemeProvider theme={darkTheme}>
       <Box
         sx={{
-          minHeight: "100dvh",
+          minHeight: "calc(var(--vh, 1vh) * 100)",
         }}
       >
         <CssBaseline />
